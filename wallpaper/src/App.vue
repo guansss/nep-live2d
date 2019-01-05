3eabf95f5a067c8c58729f9337a5d51bf607fcbd
Guan <821143943@qq.com>
2019-01-05 19:18:29 +0800

    Remove Vue template and keep minimal Vue usage

--- wallpaper/src/App.vue
+++ wallpaper/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <Settings/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Settings from './components/Settings';
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Settings
+    }
 };
 </script>
 <style lang="stylus">
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    color #333
 </style>
---
+++ wallpaper/src/components/Settings/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="settings">
+        Settings
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Settings'
+};
+</script>
+
+<style scoped lang="stylus">
+
+</style>
