f1818281fc85d2078c90be83ff2f1646df07e348
Guan <821143943@qq.com>
2019-01-08 23:54:11 +0800

    Create minimal pixi-live2d usage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
     <div id="app">
         <Settings/>
+        <div ref="canvas"></div>
     </div>
 </template>
 
 <script>
 import Settings from './components/Settings/index';
+import 'live2d.min.js';
+import * as PIXI from 'pixi.js';
+import 'pixi-live2d';
+import modelJson from 'raw-loader!live2d/neptune/normal.model.json';
 
 export default {
     name: 'app',
     components: {
         Settings
+    },
+    mounted() {
+        const renderer = new PIXI.WebGLRenderer(800, 600);
+        this.$refs.canvas.appendChild(renderer.view);
+        const stage = new PIXI.Container();
+
+        const live2dSprite = new PIXI.Live2DSprite(modelJson);
+        stage.addChild(live2dSprite);
+
+        live2dSprite.startRandomMotion('idle');
+        live2dSprite.on('mousemove', (evt) => {
+            const point = evt.data.global;
+            live2dSprite.setViewPoint(point.x, point.y);
+        });
+
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(stage);
+        }
+
+        animate();
     }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 
 module.exports = {
     productionSourceMap: false,
+    configureWebpack: {
+        externals: {
+            'live2d.min.js': '',
+            'raw-loader!live2d/neptune/normal.model.json': ''
+        }
+    },
     devServer: {
         contentBase: path.resolve('wallpaper'),
         historyApiFallback: false
