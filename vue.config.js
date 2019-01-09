db38367174eea7c151c15d9552148574fcafe525
Guan <821143943@qq.com>
2019-01-09 22:06:20 +0800

    Remove webpack externals, add PIXI alias for pixi-live2d

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,10 @@ const path = require('path');
 module.exports = {
     productionSourceMap: false,
     configureWebpack: {
-        externals: {
-            'live2d.min.js': '',
-            'raw-loader!live2d/neptune/normal.model.json': ''
+        resolve: {
+            alias: {
+                PIXI: 'pixi.js'
+            }
         }
     },
     devServer: {
