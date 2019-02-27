2f0989e5ac2215d9f9f15ad1f6179e8a986f3b67
guansss <821143943@qq.com>
2019-02-27 17:57:55 +0800

    Remove unused webpack option

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,6 @@ const path = require('path');
 
 module.exports = {
     productionSourceMap: false,
-    configureWebpack: {
-        resolve: {
-            alias: {
-                'pixi-live2d': path.resolve('pixi-live2d'),
-                PIXI: 'pixi.js',
-            },
-        },
-    },
     devServer: {
         contentBase: path.resolve('wallpaper'),
         historyApiFallback: false,
