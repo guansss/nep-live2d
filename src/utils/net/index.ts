3643293924af8052c5e22c2585f72c402c01584a
guansss <821143943@qq.com>
2019-03-13 00:15:33 +0800

    Migrate net utility to TypeScript

--- src/utils/net/index.ts
+++ src/utils/net/index.ts
@@ -5,28 +5,28 @@ const TYPE_ARRAY_BUFFER = Symbol();
 
 const log = logger('net');
 
-async function getJSON(url) {
+async function getJSON(url: string) {
     const json = await ajax(url, { type: TYPE_JSON });
 
-    log('Loaded JSON ', url, json);
+    log('Loaded JSON', url, json);
 
     return json;
 }
 
-async function getArrayBuffer(url) {
+async function getArrayBuffer(url: string) {
     const arrayBuffer = await ajax(url, { type: TYPE_ARRAY_BUFFER });
 
-    log('Loaded ArrayBuffer ', url, arrayBuffer.length);
+    log(`Loaded ArrayBuffer(${arrayBuffer.length})`, url);
 
     return arrayBuffer;
 }
 
-async function ajax(url, { type }) {
+async function ajax(url: string, { type }: { type?: symbol } = {}) {
     const res = await fetch(url);
 
     // status 0 for loading a local file
     if (!(res.status === 0 || res.status === 200)) {
-        log.error('Failed to load JSON:', url, `(${res.status})`);
+        log.error('Failed to load', url, `(${res.status})`);
     }
 
     let result;
