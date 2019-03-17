622f1a2b759735cdfd7e13849c85eb5796187813
guansss <821143943@qq.com>
2019-03-17 16:28:43 +0800

    Optimize the approach to referencing to Pixi types

--- src/core/pixi/pixi.d.ts
+++ src/core/pixi/pixi.d.ts
@@ -1,13 +1,12 @@
 /**
  * Pixi doesn't provide type definitions for individual packages,
- * so I managed to reference the types to its main declaration file in this way.
+ * so this is the temporary way to reference to types declared
+ * in its main declaration file.
  *
  * This file will be used until module definitions are officially supported.
  * @see https://github.com/pixijs/pixi.js/issues/5397
  */
 
 declare module '@pixi/display' {
-    import { Container as _Container } from 'pixi.js';
-
-    export class Container extends _Container {}
+    export { Container } from 'pixi.js';
 }
