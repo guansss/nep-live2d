8bcee776a00cfaad42f12da29b26c925cdfff152
guansss <821143943@qq.com>
2019-03-17 21:41:00 +0800

    Add TypeScript support to Jest

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = api => {
                           },
                       },
                   ],
+                  '@babel/preset-typescript',
               ]
             : ['@vue/app'],
 
