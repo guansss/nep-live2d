ed0a2a51152aa65543622fe3c1567553d4cdc0db
guansss <821143943@qq.com>
2019-08-11 03:13:13 +0800

    Load models from env configs

--- src/module/live2d/index.ts
+++ src/module/live2d/index.ts
@@ -1,11 +1,32 @@
 import { App, Module } from '@/App';
+import { error, Tagged } from '@/core/utils/log';
 import Live2DPlayer from '@/module/live2d/Live2DPlayer';
+import { resolve as urlResolve } from 'url';
 
-export default class Live2DModule implements Module {
+export default class Live2DModule implements Module, Tagged {
+    tag = Live2DModule.name;
+
     player!: Live2DPlayer;
 
     install(app: App): void {
         this.player = new Live2DPlayer(app.mka!);
         app.mka!.addPlayer('live2d', this.player);
+
+        this.loadModels();
+    }
+
+    loadModels() {
+        console.log(process.env);
+        const basePath = process.env.VUE_APP_LIVE2D_PATH + '/';
+        const models = process.env.VUE_APP_LIVE2D_MODELS;
+
+        if (basePath && models) {
+            models.split(',').forEach(model => {
+                this.player.addSprite(urlResolve(basePath, model)).catch(e => {
+                    // TODO: Show friendly error message
+                    error(this, e);
+                });
+            });
+        }
     }
 }
