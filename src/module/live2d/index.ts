23abb673a1fd27755a35ccb23c9cfeb3b96f13ad
guansss <821143943@qq.com>
2019-08-10 18:48:39 +0800

    Remove useless Live2D Vue component

--- src/module/live2d/index.ts
+++ src/module/live2d/index.ts
@@ -1,8 +1,6 @@
 import { App, Module } from '@/App';
-import Live2D from 'Live2D.vue';
 
 export default class Live2DModule implements Module {
     install(app: App): void {
-        app.addComponent(Live2D);
     }
 }
