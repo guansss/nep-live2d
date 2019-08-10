7c8f313f52bbbb191a75c267b8a4b482c815f030
guansss <821143943@qq.com>
2019-08-11 01:45:31 +0800

    Add module registration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueApp from './VueApp';
 import { App } from './App';
 import { init as initGLMW } from 'glmw';
+import Modules from './module';
 
 Vue.config.productionTip = false;
 
@@ -9,6 +10,7 @@ async function initApp(vueApp) {
     try {
         await initGLMW();
         const app = new App(vueApp);
+        Modules.forEach(Module => app.use(new Module()));
     } catch (e) {
         // TODO: Show friendly error message
         console.error(e);
---
+++ src/module/index.ts
@@ -0,0 +1,3 @@
+import Live2DModule from '@/module/live2d';
+
+export default [Live2DModule];
