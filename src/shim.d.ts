f912399b42981e047eee8e782a0c3c1f2fd744d8
guansss <821143943@qq.com>
2019-08-10 17:58:47 +0800

    Refactor matrices

--- src/shim.d.ts
+++ src/shim.d.ts
@@ -18,3 +18,52 @@ declare module '@pixi/app' {
 declare module '@pixi/display' {
     export { DisplayObject } from 'pixi.js';
 }
+
+/**
+ * glmw shims
+ */
+declare module 'glmw' {
+    type WASMAddress = number;
+    type MAT4 = WASMAddress;
+    type VEC3 = WASMAddress;
+
+    class Base {
+        static create(): WASMAddress;
+
+        static clone(a: WASMAddress): WASMAddress;
+
+        static identity(out: WASMAddress): WASMAddress;
+
+        static view(address: WASMAddress): Float32Array;
+    }
+
+    export class mat4 extends Base {
+        static scale(out: MAT4, a: MAT4, v: VEC3): MAT4;
+
+        static fromTranslation(out: MAT4, v: VEC3): MAT4;
+
+        static fromScaling(out: MAT4, v: vec3): MAT4;
+
+        static lookAt(out: MAT4, eye: VEC3, center: VEC3, up: VEC3): MAT4;
+
+        static ortho(
+            out: MAT4,
+            left: number,
+            right: number,
+            bottom: number,
+            top: number,
+            near: number,
+            far: number,
+        ): MAT4;
+
+        static translate(out: MAT4, a: MAT4, v: VEC3): MAT4;
+
+        static mul(out: MAT4, a: MAT4, b: MAT4): MAT4;
+    }
+
+    export class vec3 extends Base {
+        static fromValues(x: number, y: number, z: number): VEC3;
+
+        static transformMat4(out: VEC3, a: VEC3, m: MAT4): VEC3;
+    }
+}
