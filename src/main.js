41afc691d51099f6ab0a1431f0a40ff40dd6b503
guansss <821143943@qq.com>
2019-08-22 21:16:00 +0800

    Discard glmw

--- src/main.js
+++ src/main.js
@@ -1,27 +1,16 @@
 import Vue from 'vue';
 import VueApp from './VueApp';
 import { App } from './App';
-import { init as initGLMW } from 'glmw';
 import Modules from './module';
 
 Vue.config.productionTip = false;
-
-async function initApp(vueApp) {
-    try {
-        await initGLMW();
-        const app = new App(vueApp);
-        Modules.forEach(Module => app.use(new Module()));
-    } catch (e) {
-        // TODO: Show friendly error message
-        console.error(e);
-    }
-}
 
 new Vue({
     render(h) {
         const vueApp = h(VueApp);
 
-        initApp(vueApp).then();
+        const app = new App(vueApp);
+        Modules.forEach(Module => app.use(new Module()));
 
         return vueApp;
     },
