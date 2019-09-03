297faabede53b09a3b392e3f865f835fd988c55f
guansss <821143943@qq.com>
2019-09-04 03:40:36 +0800

    Remove everything about unit testing
    Stupid test. Why the hell was I wasting my life on it?

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,3 @@
-module.exports = api => {
-    const inTest = api.env('test');
-
-    return {
-        presets: inTest
-            ? [
-                  [
-                      '@babel/preset-env',
-                      {
-                          targets: {
-                              node: 'current',
-                          },
-                      },
-                  ],
-                  '@babel/preset-typescript',
-              ]
-            : ['@vue/app'],
-
-        plugins: ['@babel/plugin-proposal-class-properties'],
-    };
+module.exports = {
+    presets: ['@vue/app'],
 };
