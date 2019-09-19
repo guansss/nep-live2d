e6c9c53505feeb801daad3f8a2d9c1d6f8fd28dd
guansss <821143943@qq.com>
2019-09-20 01:51:58 +0800

    Prettify background image selector

--- src/module/config/settings/BackgroundSettings.vue
+++ src/module/config/settings/BackgroundSettings.vue
@@ -2,15 +2,25 @@
     <div class="page">
         <FileInput @change="imageInputChange" />
 
-        <div v-for="(image, i) in images" :key="i" class="bg-list" @click="selectImage(image)">
-            <img
-                v-if="image.valid"
-                class="bg-item-img"
-                :title="image.name"
-                :src="imageDir + '/' + image.name"
-                @load="imageLoaded(image)"
-                @error="imageError(image)"
-            />
+        <div class="bg-list">
+            <div
+                v-for="(image, i) in images"
+                :key="i"
+                :class="['bg-item card', { selected: i === selected }]"
+                @click="selectImage(image, i)"
+            >
+                <img
+                    v-if="image.valid"
+                    class="bg-item-img"
+                    :title="image.name"
+                    :src="imageDir + '/' + image.name"
+                    @load="imageLoaded(image)"
+                    @error="imageError(image)"
+                />
+                <svg class="check" viewBox="0 0 24 24">
+                    <path fill="#FFF" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+                </svg>
+            </div>
         </div>
     </div>
 </template>
@@ -43,12 +53,16 @@ export default class BackgroundSettings extends Vue {
 
     images: ImageEntity[] = [];
 
+    selected = -1;
+
     private created() {
         this.images = (this.configModule.getConfig('bg.images', []) as BackgroundImage[]).map(({ name }) => ({
             name,
             valid: true,
             saved: true,
         }));
+
+        this.selected = this.configModule.getConfig('bg.selected', this.selected);
     }
 
     imageInputChange(e: Event) {
@@ -66,6 +80,9 @@ export default class BackgroundSettings extends Vue {
     imageLoaded(image: ImageEntity) {
         if (!image.saved) {
             this.configModule.app.emit('bgSave', image.name);
+
+            // let's assume that it's been saved...
+            image.saved = true;
         }
     }
 
@@ -73,18 +90,67 @@ export default class BackgroundSettings extends Vue {
         image.valid = false;
     }
 
-    selectImage(image: ImageEntity) {
-        this.configModule.app.emit('bgSelect', image.name);
+    selectImage(image: ImageEntity, index: number) {
+        if (image.saved && image.valid) {
+            this.configModule.app.emit('bgSelect', image.name);
+
+            this.selected = index;
+        }
     }
 }
 </script>
 
 <style scoped lang="stylus">
 .bg-list
+    display grid
+    padding 16px
+    width 100%
+    height 100%
+    grid-template-columns repeat(3, 1fr)
+    grid-gap 8px
+
+.bg-item
     position relative
     cursor pointer
 
+    &.selected
+        .check
+            opacity 1
+
+    &:before
+        content ''
+        display block
+        padding-bottom (9 / 16) * 100%
+
+    &:hover
+
+        .bg-item-img
+            transform scale(1.1)
+
 .bg-item-img
+    position absolute
     display block
-    width 200px
+    top 0
+    left 0
+    width 100%
+    height 100%
+    object-fit cover
+    transition transform .15s ease-out
+
+.check
+    position absolute
+    display block
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background #0004
+    opacity 0
+    object-fit cover
+    transition opacity .2s
+
+.card
+    overflow hidden
+    border-radius 4px
+    box-shadow 0 3px 1px -2px #0003, 0 2px 2px 0px #0002, 0 1px 5px 0px #0002
 </style>
