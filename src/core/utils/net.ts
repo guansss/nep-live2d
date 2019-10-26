5ca92e11afed0483d6a915c5a2b22245d18099f7
guansss <821143943@qq.com>
2019-10-26 18:10:16 +0800

    Change net request implementation from fetch to XHR

--- src/core/utils/net.ts
+++ src/core/utils/net.ts
@@ -1,21 +1,15 @@
 import { error, log } from './log';
 
-const enum ResultType {
-    None,
-    JSON,
-    ArrayBuffer,
+interface RequestOptions {
+    method?: 'GET' | 'POST';
+    body?: any;
+    responseType?: XMLHttpRequestResponseType;
 }
-
-const REQUEST_OPTIONS = {
-    method: 'GET',
-    body: {},
-    type: ResultType.None,
-};
 
 const TAG = 'Net';
 
 export async function getJSON(url: string) {
-    const result = await request(url, { type: ResultType.JSON });
+    const result = await request(url, { responseType: 'json' });
 
     log(TAG, 'Loaded JSON', url, result);
 
@@ -28,7 +22,7 @@ export async function postJSON(url: string, json: any) {
     const result = await request(url, {
         method: 'POST',
         body: json,
-        type: ResultType.JSON,
+        responseType: 'json',
     });
 
     log(TAG, 'Result', url, result);
@@ -37,53 +31,33 @@ export async function postJSON(url: string, json: any) {
 }
 
 export async function getArrayBuffer(url: string) {
-    const arrayBuffer: ArrayBuffer = await request(url, { type: ResultType.ArrayBuffer });
+    const arrayBuffer = await request<ArrayBuffer>(url, { responseType: 'arraybuffer' });
 
     log(TAG, `Loaded ArrayBuffer(${arrayBuffer.byteLength})`, url);
 
     return arrayBuffer;
 }
 
-async function request(url: string, options: Partial<typeof REQUEST_OPTIONS> = {}) {
-    const _options = Object.assign({}, REQUEST_OPTIONS, options) as typeof REQUEST_OPTIONS;
+async function request<T extends any>(url: string, options: RequestOptions = {}): Promise<T> {
+    // DON'T use fetch because it refuses load local files
+    const xhr = new XMLHttpRequest();
+    xhr.open(options.method || 'GET', url);
 
-    const res = await fetch(
-        url,
-        _options.method === 'GET'
-            ? undefined
-            : {
-                method: _options.method,
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(_options.body),
-            },
-    );
+    xhr.responseType = options.responseType || '';
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+    const res = await new Promise((resolve, reject) => {
+        xhr.onload = () => resolve(xhr.response);
+        xhr.onerror = () => reject(new TypeError('Request failed'));
+
+        xhr.send(options.body && JSON.stringify(options.body));
+    });
 
     // status 0 for loading a local file
-    if (!(res.status === 0 || res.status === 200)) {
-        error(TAG, 'Failed to load', url, `(${res.status})`);
+    if (!(xhr.status === 0 || xhr.status === 200)) {
+        error(TAG, 'Failed to load', url, `(${xhr.status})`);
     }
 
-    let result;
-
-    switch (options.type) {
-        case ResultType.JSON: {
-            const resultText = await res.text();
-
-            try {
-                result = JSON.parse(resultText);
-            } catch (e) {
-                throw 'Failed to parse JSON: ' + resultText;
-            }
-            break;
-        }
-
-        case ResultType.ArrayBuffer: {
-            result = await res.arrayBuffer();
-            break;
-        }
-
-        // defaults to ignoring the result
-    }
-
-    return result;
+    return res as T;
 }
