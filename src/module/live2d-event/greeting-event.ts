5cec5ddc15a5f0699fe3965c3d14b6415264b770
guansss <821143943@qq.com>
2019-10-27 12:03:19 +0800

    Add support for timed greeting motions

--- src/module/live2d-event/greeting-event.ts
+++ src/module/live2d-event/greeting-event.ts
@@ -3,15 +3,50 @@ import { Config } from '../config/ConfigModule';
 import Live2DSprite from '../live2d/Live2DSprite';
 
 export default function greet(sprite: Live2DSprite, config: Config) {
-    const definitions = sprite.model.motionManager.definitions;
+    const definitions = sprite.model.motionManager.definitions['greet'];
 
-    if (definitions['greet']) {
+    if (definitions && definitions.length !== 0) {
         const theme = THEMES[config.get('theme.selected', -1)];
+        let index = -1;
 
         if (theme && theme.season) {
-            const index = definitions['greet'].findIndex(def => def.season === theme.season);
-
-            if (index !== -1) sprite.model.motionManager.startMotionByPriority('greet', index).then();
+            index = definitions.findIndex(def => def.season === theme.season);
         }
+
+        if (index === -1) {
+            const indices: number[] = [];
+            const hours = new Date().getHours();
+            let min = 24;
+            let dt;
+
+            // find a timed greeting motion closest the current hours
+            for (let i = definitions.length - 1; i >= 0; i--) {
+                if (!isNaN(definitions[i].time as number)) {
+                    dt = hours - definitions[i].time!;
+
+                    if (dt >= 0 && dt <= min) {
+                        if (dt === min) {
+                            indices.push(i);
+                        } else {
+                            min = dt;
+                            indices.splice(0, indices.length, i);
+                        }
+                    }
+                }
+            }
+
+            if (indices.length !== 0) {
+                index = ~~(Math.random() * indices.length);
+            } else {
+                // start a random non-seasonal greeting motion
+                const nonSeasonalIndices = Array.from(definitions.keys()).filter(i => !definitions[i].season);
+
+                if (nonSeasonalIndices.length !== 0) {
+                    index = nonSeasonalIndices[~~(Math.random() * nonSeasonalIndices.length)];
+                }
+            }
+        }
+
+        if (index !== -1) sprite.model.motionManager.startMotionByPriority('greet', index).then();
     }
 }
