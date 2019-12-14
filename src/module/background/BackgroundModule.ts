f00a3e2e43ba850071fe76de1d3f0dc853d03936
guansss <821143943@qq.com>
2019-12-15 02:00:00 +0800

    Set background via BackgroundModule on startup

--- src/module/background/BackgroundModule.ts
+++ src/module/background/BackgroundModule.ts
@@ -2,7 +2,7 @@ import { App, Module } from '@/App';
 
 export interface Config {
     bg: {
-        image?: string;
+        img?: string;
     };
 }
 
@@ -10,7 +10,11 @@ export default class BackgroundModule implements Module {
     name = 'Background';
 
     constructor(readonly app: App) {
-        app.on('config:bg.img', this.selectImage, this);
+        app.on('config:bg.img', this.selectImage, this).on('configReady', (config: Config) => {
+            if (config.bg && config.bg.img) {
+                this.selectImage(config.bg.img);
+            }
+        });
 
         document.body.style.backgroundSize = 'cover';
         document.body.style.transition = 'background .2s';
